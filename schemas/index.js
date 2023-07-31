82649061aa082eb8630fdabe9f7865f25b909e2b
eighty-nine89 <awueteysamuel222@yahoo.com>
2023-07-31 08:02:48 +0000

    update

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,9 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import dish from './dish'
+import featured from './featured'
+import restaurant from './restaurant'
+import 'react-native-url-polyfill/auto';
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [category, dish, featured, restaurant]
